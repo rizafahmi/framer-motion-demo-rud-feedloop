179f35f0af41369882acf4271219e3333d2841e1
Riza Fahmi <rizafahmi@gmail.com>
2022-05-17 15:52:19 +0700

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="p-8">
+      <div className="relative flex flex-col justify-center">
+        <div className="w-full h-2 bg-slate-300 rounded-full absolute"></div>
+        <div className="relative bg-red-400 w-6 h-6 rounded-full"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
